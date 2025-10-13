608bd868f875f73cb82885a7eb235db62a805078
Lucas Farias <luksrn@gmail.com>
2025-10-13 23:14:42 +0100

    Use ExpandingQueryTransformer and adjust ReRankingContentAggregator to use QuerySelector based on the number of documents returned

--- rag/src/main/kotlin/com/github/luksrn/pkmbridge/RagConfiguration.kt
+++ rag/src/main/kotlin/com/github/luksrn/pkmbridge/RagConfiguration.kt
@@ -2,18 +2,23 @@ package com.github.luksrn.pkmbridge
 
 import dev.langchain4j.data.document.Document
 import dev.langchain4j.data.segment.TextSegment
+import dev.langchain4j.internal.Exceptions
+import dev.langchain4j.model.chat.ChatModel
 import dev.langchain4j.model.embedding.EmbeddingModel
 import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel
 import dev.langchain4j.model.scoring.onnx.OnnxScoringModel
 import dev.langchain4j.rag.DefaultRetrievalAugmentor
 import dev.langchain4j.rag.RetrievalAugmentor
+import dev.langchain4j.rag.content.Content
 import dev.langchain4j.rag.content.aggregator.ContentAggregator
 import dev.langchain4j.rag.content.aggregator.ReRankingContentAggregator
 import dev.langchain4j.rag.content.injector.DefaultContentInjector
 import dev.langchain4j.rag.content.retriever.ContentRetriever
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever
+import dev.langchain4j.rag.query.Query
 import dev.langchain4j.rag.query.router.DefaultQueryRouter
 import dev.langchain4j.rag.query.router.QueryRouter
+import dev.langchain4j.rag.query.transformer.ExpandingQueryTransformer
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore
 import org.springframework.beans.factory.ObjectProvider
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -22,6 +27,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Component
+import java.util.function.Function
 
 @Configuration
 class RagConfiguration {
@@ -49,9 +55,11 @@ class RagConfiguration {
     fun retrievalAugment(
         queryRouter: QueryRouter,
         contentAggregatorProvider: ObjectProvider<ContentAggregator>,
+        chatModel: ChatModel
     ): RetrievalAugmentor =
         DefaultRetrievalAugmentor
             .builder()
+            .queryTransformer(ExpandingQueryTransformer(chatModel))
             .queryRouter(queryRouter)
             // when re-rank is enabled, the contentAggregatorProvider will provide the ReRankingContentAggregator bean
             .contentAggregator(contentAggregatorProvider.ifAvailable)
@@ -59,10 +67,21 @@ class RagConfiguration {
             .build()
 
     @Bean
+    @ConditionalOnProperty(prefix = "re-rank", name = ["enabled"], havingValue = "true", matchIfMissing = false)
     @ConditionalOnProperty(prefix = "re-rank", name = ["path-to-model", "path-to-tokenizer"])
     fun contentAggregator(reRankProperties: ReRankProperties): ContentAggregator =
         ReRankingContentAggregator
             .builder()
+            .querySelector(Function { queryToContents: MutableMap<Query, MutableCollection<MutableList<Content>>> ->
+                // select the query that retrieved the largest number of contents
+                var selected = queryToContents.keys.first()
+                for( query in queryToContents) {
+                    if( query.value.first().size > queryToContents[selected]!!.first().size ) {
+                        selected = query.key
+                    }
+                }
+                selected
+            })
             .scoringModel(OnnxScoringModel(reRankProperties.pathToModel, reRankProperties.pathToTokenizer))
             .maxResults(reRankProperties.maxResult)
             .minScore(reRankProperties.minScore)
