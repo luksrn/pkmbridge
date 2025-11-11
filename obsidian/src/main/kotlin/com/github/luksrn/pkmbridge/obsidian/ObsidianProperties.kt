04b436f164c01ebfde6d47d57048c2a54e98b9b5
Thiago Resende <thiago.gcresende@gmail.com>
2025-11-10 21:32:45 -0300

    feat(obsidian): enhance document transformer with properties and add vault retrieval

--- obsidian/src/main/kotlin/com/github/luksrn/pkmbridge/obsidian/ObsidianProperties.kt
+++ obsidian/src/main/kotlin/com/github/luksrn/pkmbridge/obsidian/ObsidianProperties.kt
@@ -1,9 +1,13 @@
 package com.github.luksrn.pkmbridge.obsidian
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import kotlin.io.path.Path
 
 @ConfigurationProperties(prefix = "pkm.obsidian")
 data class ObsidianProperties(
     var enabled: Boolean = true,
     var fileSystemPath: String = "",
-)
+) {
+    fun getVault() =
+        Path(fileSystemPath).fileName.toString()
+}
